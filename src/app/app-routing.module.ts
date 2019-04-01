bb6ce2c23c0fe23de9489f8353fec34a9be7750a
harshit740 <harsh.008.com@gmail.com>
2019-04-01 20:36:26 +0530

    Fixed ;_;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { BlogComponent } from './home/blog/blog.component';
 import { AboutComponent } from './home/about/about.component';
 import { AdminComponent } from './admin/admin.component';
 import { PagesComponent } from './admin/pages/pages.component';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
 const routes: Routes = [
   {
@@ -16,7 +17,7 @@ const routes: Routes = [
       loadChildren: './home/home.module#HomeModule'
     },
     {
-      path:'',component:HomeComponent
+      path:'',component:HomepageComponent
   },
     {
       path : 'blog',
