418c642a3d3976eaaedaa3af7e6b12a9cf901c42
harshit740 <harsh.008.com@gmail.com>
2019-04-03 00:30:15 +0530

    added bllog to homepage

--- src/app/admin/blog/bloglist/bloglist.component.ts
+++ src/app/admin/blog/bloglist/bloglist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BlogService } from '../../../services/blog.service'
+import { Router } from '@angular/router';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-bloglist',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bloglist.component.css']
 })
 export class BloglistComponent implements OnInit {
-
-  constructor() { }
+  bloglist
+  constructor(private list:BlogService , private router:Router,private rout:ActivatedRoute) { }
 
   ngOnInit() {
+    this.list.bloglist().subscribe(
+      data => {
+        this.bloglist = data;
+      })
   }
 
 }
