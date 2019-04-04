d5f889fd06ef34debbe66e903317c35c6959ab9c
harshit740 <harsh.008.com@gmail.com>
2019-04-04 12:47:19 +0530

    Login Page Added

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router,RouterModule } from '@angular/router';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 
@@ -8,24 +8,18 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit  {
-  public errorMessage = "null";
-  public loginForm: FormGroup;
-    ngOnInit(): void {
-    this.loginForm = this.fb.group ( {
-      usernname: [null , Validators.compose ( [ Validators.required ] )] , password: [null , Validators.compose ( [ Validators.required ] )]
-    } );
-    throw new Error("Method not implemented.");
-  } 
-     
-email = 'peter@klaven';
-password = 'cityslicka';
-
-constructor( private router: Router,private fb: FormBuilder) {
+export class LoginComponent   {
+loginForm: FormGroup;
+constructor(private fb: FormBuilder ,private router:Router) {
+ this.createForm();
 }
-
-onSubmit() {
-  this.errorMessage = 'network problem';
-}
+createForm() {
+ this.loginForm = this.fb.group({
+    username: ['', Validators.required ],
+    password: ['', Validators.required ]
+ });}
+ onSubmit(){
+   console.log(this.loginForm.value.username,this.loginForm.value.password)
+ }
 
 }
