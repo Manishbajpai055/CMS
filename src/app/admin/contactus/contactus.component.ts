580de006e32c335c7e0adf53d6e9d02ee5aae17a
manishbjp <www.manishbajpai055@gmail.com>
2019-04-07 17:00:25 +0530

    Subscriber form and API

--- src/app/admin/contactus/contactus.component.ts
+++ src/app/admin/contactus/contactus.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ContactusService } from 'src/app/services/contactus.service';
+
 
 @Component({
   selector: 'app-contactus',
@@ -7,8 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Contactus:ContactusService) { }
 
+  subscribe: FormGroup = new FormGroup({
+    Name: new FormControl('' ),
+    Email: new FormControl(''),
+    Phone_no : new FormControl(''),
+    Description : new FormControl(''),
+});
+
+onSubmit(){
+  console.log(this.subscribe.value)
+  this.Contactus.postSubscriberinfo(this.subscribe.value)
+}
   ngOnInit() {
   }
 
---
+++ src/app/services/contactus.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactusService {
+
+  constructor(private http:HttpClient) { }
+  
+  postSubscriberinfo(data){
+    return this.http.post('http://localhost:3004/subscriber/',data).subscribe(res=>{
+    })
+}
+}
