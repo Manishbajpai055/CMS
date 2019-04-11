e0ee333c7dd687823492b5a22c06129248a38b29
harshit740 <harsh.008.com@gmail.com>
2019-04-11 14:45:41 +0530

    Conflict

--- src/app/admin/Student/answerss/new-answers/new-answers.component.ts
+++ src/app/admin/Student/answerss/new-answers/new-answers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-answers',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-answers.component.css']
 })
 export class NewAnswersComponent implements OnInit {
+  submitted: boolean;
 
-  constructor() { }
-
+  constructor(private formBuilder:FormBuilder) { }
+  NewAnswers:FormGroup
   ngOnInit() {
+    this.NewAnswers = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      answer:['',Validators.required],  
+  })
   }
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    this.submitted = true;
+    console.log(this.NewAnswers.value)
+    if (this.NewAnswers.invalid) {
+      return;
+    }
+  }
+  get f() { return this.NewAnswers.controls; }
 
 }
