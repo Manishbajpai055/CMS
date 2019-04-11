6fea0512cf2dc2e9302f9e1bec85417fede9f73c
harshit740 <harsh.008.com@gmail.com>
2019-04-11 12:22:12 +0530

    User Delete module added

--- src/app/admin/Student/users/userlist/userlist.component.ts
+++ src/app/admin/Student/users/userlist/userlist.component.ts
@@ -29,9 +29,14 @@ export class UserlistComponent implements OnInit {
   onSubmit() {
     // TODO: Use EventEmitter with form value
     this.submitted = true;
+    console.log(this.NewUser.value)
     if (this.NewUser.invalid) {
       return;
     }
+    this.userservice.NewUser(this.NewUser.value).subscribe(res=>{
+      this.usercomponent.useradd=false
+      this.usercomponent.userlist = true
+    })
   }
   goBack(){
    this.usercomponent.useradd=false
--- src/app/admin/Student/users/users.component.ts
+++ src/app/admin/Student/users/users.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { UserserviceService } from 'src/app/services/users/userservice.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -10,17 +12,38 @@ export class UsersComponent implements OnInit {
   useradd = false
   userlist = true
   UsersList
-  constructor(private userservice:UserserviceService) { }
+  modalRef: BsModalRef;
+  id
+  constructor(private userservice:UserserviceService,private router: Router,private modalService: BsModalService) { }
  
   ngOnInit() {
-    this.userservice.UsersList().subscribe(res =>{
-      this.UsersList = res
-      console.log(res)
-    })
+    this.refresh()
   }
   add_newUser(){
     this.useradd = true
     this.userlist = false
   }
-
+  delet(id,template){
+     this.id=id
+     this.openModal(template)
+  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+  confirm(): void {
+    this.userservice.DeleteUser(this.id).subscribe(res=>{
+      console.log("deleted")
+    })
+    this.modalRef.hide();
+    this.refresh()
+  }
+  refresh(){
+    this.userservice.UsersList().subscribe(res =>{
+      this.UsersList = res
+      console.log(res)
+    })  
+  }
+  decline(): void {
+    this.modalRef.hide();
+  }
 }
