501ffaafa03814b012d18b9d6d4951773b48b38c
manishbjp <www.manishbajpai055@gmail.com>
2019-04-26 18:26:16 +0530

    layout issue

--- src/app/admin/contactus/contactus.component.ts
+++ src/app/admin/contactus/contactus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ContactusService } from 'src/app/services/contactus.service';
@@ -12,34 +12,44 @@ import { HeaderFooterService } from 'src/app/services/header-footer.service';
 })
 export class ContactusComponent implements OnInit {
   headerinfo
+  htmlToAdd: string;
 
-  constructor(private headerfooterservice:HeaderFooterService, private Contactus:ContactusService,private formBuilder: FormBuilder) { }
-  subscribeform: FormGroup 
+  constructor(private elementRef: ElementRef, private headerfooterservice: HeaderFooterService, private Contactus: ContactusService, private formBuilder: FormBuilder) { }
+  subscribeform: FormGroup
   submitted = false;
 
-onSubmit(){
-  console.log(this.subscribeform.value)
-  this.submitted = true;
+  onSubmit() {
+    console.log(this.subscribeform.value)
+    this.submitted = true;
     if (this.subscribeform.invalid) {
-        return;
+      return;
     }
     this.Contactus.postSubscriberinfo(this.subscribeform.value)
     alert('Success :) ')
-}
+  }
   ngOnInit() {
     this.refresh()
     this.subscribeform = this.formBuilder.group({
-      name: ['', [Validators.required, Validators.minLength(4),Validators.maxLength(100), Validators.pattern("^[A-Z a-z]*$")],],
+      name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(100), Validators.pattern("^[A-Z a-z]*$")],],
       email: ['', [Validators.required, Validators.email]],
-      phone_no: ['', [Validators.required, Validators.minLength(10),Validators.maxLength(10), Validators.pattern("^[0-9]*$")],],
+      phone_no: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern("^[0-9]*$")],],
       description: ['', Validators.required]
-  })
-   }
+    })
+
+
+    var d1 = this.elementRef.nativeElement.querySelector('.one');
+    d1.insertAdjacentHTML('beforeend', '<div class="two">two</div>');
+
+    if (window.screen.width <= 760) { // 768px portrait
+      this.htmlToAdd = '<span class="divider">&nbsp;</span>'
+    }
+
+}
   get f() { return this.subscribeform.controls; }
-  refresh(){
-    this.headerfooterservice.getHeadeFooter().subscribe(res=>{
+  refresh() {
+    this.headerfooterservice.getHeadeFooter().subscribe(res => {
       console.log(res)
-      this.headerinfo=res
+      this.headerinfo = res
     })
   }
 }
