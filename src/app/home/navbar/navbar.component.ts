81a5f38d8ff473def3c39398858a51696ee7011d
harshit740 <harsh.008.com@gmail.com>
2019-05-23 12:25:07 +0530

    added synamic site name on navbar

--- src/app/home/navbar/navbar.component.ts
+++ src/app/home/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PageserviceService } from 'src/app/services/pageservice.service';
 import { Router } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
+import { AboutService } from 'src/app/services/about.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,11 +11,12 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   pagelist
+  sitename
   username = 's'
   loged_in:Boolean
   admin 
   student = false
-  constructor(private list:PageserviceService,private router:Router) { }
+  constructor(private list:PageserviceService,private router:Router,private sitemetdata:AboutService) { }
 
   ngOnInit() {
     
@@ -24,6 +27,9 @@ export class NavbarComponent implements OnInit {
     this.router.navigate(['page',slug]);
   }
   refresh(){
+    this.sitemetdata.About().subscribe(res =>{
+      this.sitename = res['sitename']
+    })
     if (localStorage.getItem('token')) {
       this.loged_in = true
       this.username = localStorage.getItem('user')
